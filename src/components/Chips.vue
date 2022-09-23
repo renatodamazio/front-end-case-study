398f2f5509243c05499cdc1ed905cd3bb88c6b45
Renato Damázio <renatopdamazio@gmail.com>
2022-09-23 08:07:44 -0300

    Chips components for small piece of information

---
+++ src/components/Chips.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="ui-chips">
+    <slot></slot>
+  </div>
+</template>
+<style lang="scss">
+.ui-chips {
+  display: flex;
+  height: var(--ui-chips-height);
+  align-items: center;
+  justify-content: center;
+  font-size: var(--ui-chips-fs);
+  line-height: var(--ui-chips-line-height);
+  font-weight: var(--ui-chips-fw);
+  background-color: var(--ui-chips-bg);
+  border-radius: var(--ui-chips-border-radius);
+  width: fit-content;
+  padding: 0 var(--ui-chips-padding);
+  white-space: nowrap;
+}
+</style>
+<script lang="ts">
+export default {
+  name: "chips",
+};
+</script>
