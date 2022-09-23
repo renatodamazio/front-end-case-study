4dfe626aee39fc7fb986309cf3b2a2b3e8e6eaeb
Renato Damázio <renatopdamazio@gmail.com>
2022-09-23 08:06:59 -0300

    TheTags component wrapper Chips Components

---
+++ src/components/TheTags.vue
@@ -0,0 +1,26 @@
+<template>
+   <div v-for="tag, index in _tags">
+        <Chips :key="index">{{tag}}</Chips>
+    </div>
+</template>
+<script lang="ts">
+import Chips from "./Chips.vue";
+
+export default {
+  name: "TheTags",
+  props: ["tags"],
+  components: {
+    Chips,
+  },
+
+  data() {
+    return {
+        _tags: []
+    }
+  },
+
+  mounted() {
+    this._tags =  this.tags ? this.tags.split(",") : [];
+  }
+};
+</script>
